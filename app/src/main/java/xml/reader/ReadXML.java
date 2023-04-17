6fdbe073ddd61b1cd91a619706de4caa661d201e
Panashe Matirekwe <matirekwe3@gmail.com>
2023-04-18 01:32:49 +0200

    initial commit

---
+++ app/src/main/java/xml/reader/ReadXML.java
@@ -0,0 +1,45 @@
+package xml.reader;
+import java.io.File;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.DocumentBuilder;
+import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
+import org.w3c.dom.Node;
+import org.w3c.dom.Element;
+
+public class ReadXML {
+    public static void main(String[] args) {
+        try {
+            // create a new file object
+            File file = new File("data.xml");
+            
+            // create a new DocumentBuilder
+            DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+            
+            // parse the XML file into a Document object
+            Document doc = dBuilder.parse(file);
+            
+            // normalize the Document object
+            doc.getDocumentElement().normalize();
+            
+            // get a list of "person" elements
+            NodeList nodeList = doc.getElementsByTagName("person");
+            
+            // loop through the "person" elements and print out the field values
+            for (int i = 0; i < nodeList.getLength(); i++) {
+                Node node = nodeList.item(i);
+                
+                if (node.getNodeType() == Node.ELEMENT_NODE) {
+                    Element element = (Element) node;
+                    String name = element.getElementsByTagName("name").item(0).getTextContent();
+                    String age = element.getElementsByTagName("age").item(0).getTextContent();
+                    System.out.println("Name: " + name);
+                    System.out.println("Age: " + age);
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
